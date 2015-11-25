b7763042dcca39614c5a938825145eb4f31b59ae
Arturo Herrera Vargas <arturohv@gmail.com>
2015-11-25 16:04:33 -0600

    Configuracion con StormPHP

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://localhost:8080',
 
     /*
     |--------------------------------------------------------------------------
@@ -144,6 +144,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
